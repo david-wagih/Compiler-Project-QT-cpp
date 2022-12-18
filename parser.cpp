62c1114bd5924d7c07aadbeb924d5ce9bd4456cd
mathewehab <meshokralla@gmail.com>
2022-12-18 14:59:44 +0200

    Complete parser logic

--- parser.cpp
+++ parser.cpp
@@ -131,3 +131,163 @@ Node* repeat_stmt(queue<Token>* q) {
 	}
 	return root;
 }
+
+Node *assign_stmt(queue<Token> *q) {
+	Node *root = nullptr;
+	Node *child;
+	Token assign;
+	Token error;
+	error.type = "ERROR";
+	if (q->front().type == "IDENTIFIER") {
+		assign.value = q->front().value;
+		q->pop();
+		if (q->front().type == "ASSIGN") {
+			assign.type = q->front().type;
+			q->pop();
+			root = newNode(assign);
+			child = exp(q);
+			if (child->key.type == "ERROR") root = child;
+			else root->child.push_back(child);
+		}
+		else {
+			error.type = "invalid assign syntax";
+			root = newNode(error);
+		}
+	}
+	return root;
+}
+
+Node *read_stmt(queue<Token> *q) {
+	Node *root = nullptr;
+	Token r;
+	Token error;
+	error.type = "ERROR";
+	if (q->front().type == "READ") {
+		r.type = "READ";
+		q->pop();
+		if (q->front().type == "IDENTIFIER") {
+			r.value = q->front().value;
+			q->pop();
+			root = newNode(r);
+		}
+		else {
+			error.value = "invalid read syntax";
+			root = newNode(error);
+		}
+	}
+	return root;
+}
+
+Node *write_stmt(queue<Token> *q) {
+	Node *root = nullptr;
+	Node *child;
+	if (q->front().type == "WRITE") {
+		root = newNode(q->front());
+		q->pop();
+		child = exp(q);
+		if (child->key.type == "ERROR") root = child;
+		else root->child.push_back(child);
+	}
+	return root;
+}
+
+Node *exp(queue<Token> *q) {
+	Node *simple_1;
+	simple_1 = simple_exp(q);
+	Node *op = simple_1;
+	if (simple_1->key.type != "ERROR") {
+		if (!q->empty() && (q->front().value == "<" || q->front().value == "=")) {
+			Token comparison_op;
+			comparison_op.type = q->front().type;
+			comparison_op.value = q->front().value;
+			op = newNode(comparison_op);
+			q->pop();
+			Node *simple_2;
+			simple_2 = simple_exp(q);
+			op->child.push_back(simple_1);
+			if (simple_2->key.type == "ERROR") op = simple_2;
+			else op->child.push_back(simple_2);
+		}
+	}
+	return op;
+}
+
+Node *simple_exp(queue<Token> *q) {
+	Node *term_1;
+	term_1 = term(q);
+	if (term_1->key.type != "ERROR") {
+		while (!q->empty() && (q->front().value == "+" || q->front().value == "-")) {
+			Token add_op;
+			add_op.type = q->front().type;
+			add_op.value = q->front().value;
+			Node *add_op_node = newNode(add_op);
+			q->pop();
+			Node *term_2;
+			term_2 = term(q);
+			add_op_node->child.push_back(term_1);
+			if (term_2->key.type == "ERROR") {
+				term_1 = term_2;
+				break;
+			}
+			else {
+				add_op_node->child.push_back(term_2);
+				term_1 = add_op_node;
+			}
+		}
+	}
+	return term_1;
+}
+
+Node *term(queue<Token> *q) {
+	Node *root = nullptr;
+	Node *left;
+	Node *right;
+	Token op;
+	root = factor(q);
+	if (root->key.type != "ERROR") {
+		while (!q->empty() && (q->front().type == "MULT" || q->front().type == "DIV")) {
+			left = root;
+			op.value = q->front().value;
+			op.type = "OPERATOR";
+			root = newNode(op);
+			root->child.push_back(left);
+			q->pop();
+			right = factor(q);
+			if (right->key.type == "ERROR") {
+				root = right;
+				break;
+			}
+			else root->child.push_back(right);
+		}
+	}
+	return root;
+}
+
+Node *factor(queue<Token> *q) {
+	Node *root = nullptr;
+	Token temp;
+	Token error;
+	error.type = "ERROR";
+	if (q->front().type == "OPENBRACKET") {
+		q->pop();
+		root = exp(q);
+		if (q->front().type == "CLOSEDBRACKET") q->pop();
+		else error.value = "missing closed brackets";
+		root = newNode(error);
+	}
+	else if (q->front().type == "NUMBER") {
+		temp.type = "Const";
+		temp.value = q->front().value;
+		root = newNode(temp);
+		q->pop();
+	}
+	else if (q->front().type == "IDENTIFIER") {
+		root = newNode(q->front());
+		q->pop();
+	}
+	else {
+		error.value = "invalid syntax";
+		root = newNode(error);
+	}
+	return root;
+}
